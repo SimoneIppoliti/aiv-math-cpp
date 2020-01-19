97624acfe8b226256292e66788d8684e725a377f
Simone Ippoliti <simoneippoliti53@gmail.com>
2020-01-19 19:38:09 +0100

    Correct minor issues in Point class

--- point.cpp
+++ point.cpp
@@ -6,134 +6,134 @@ Point::Point(const float _x, const float _y) : x{_x}, y{_y} {}
 
 Point::Point(const Point &other) : x{other.x}, y{other.y} {}
 
-Point & Point::operator=(const Point &other) {
-    x = other.x;
-    y = other.y;
-    return *this;
+Point &Point::operator=(const Point &other) {
+	x = other.x;
+	y = other.y;
+	return *this;
 }
 
 Point Point::operator+(const float value) const {
-    Point p;
-    p.x = x + value;
-    p.y = y + value;
-    return p;
+	Point p;
+	p.x = x + value;
+	p.y = y + value;
+	return p;
 }
 
 Point Point::operator+(const Point &other) const {
-    Point p;
-    p.x = x + other.x;
-    p.y = y + other.y;
-    return p;
+	Point p;
+	p.x = x + other.x;
+	p.y = y + other.y;
+	return p;
 }
 
-Point & Point::operator+=(const float value) {
-    x += value;
-    y += value;
-    return *this;
+Point &Point::operator+=(const float value) {
+	x += value;
+	y += value;
+	return *this;
 }
 
-Point & Point::operator+=(const Point &other) {
-    x += other.x;
-    y += other.y;
-    return *this;
+Point &Point::operator+=(const Point &other) {
+	x += other.x;
+	y += other.y;
+	return *this;
 }
 
 Point Point::operator-(const float value) const {
-    Point p;
-    p.x = x - value;
-    p.y = y - value;
-    return p;
+	Point p;
+	p.x = x - value;
+	p.y = y - value;
+	return p;
 }
 
 Point Point::operator-(const Point &other) const {
-    Point p;
-    p.x = x - other.x;
-    p.y = y - other.y;
-    return p;
+	Point p;
+	p.x = x - other.x;
+	p.y = y - other.y;
+	return p;
 }
 
-Point & Point::operator-=(const float value) {
-    *this += -value;
-    return *this;
+Point &Point::operator-=(const float value) {
+	*this += -value;
+	return *this;
 }
 
-Point & Point::operator-=(const Point &other) {
-    x -= other.x;
-    y -= other.y;
-    return *this;
+Point &Point::operator-=(const Point &other) {
+	x -= other.x;
+	y -= other.y;
+	return *this;
 }
 
 Point Point::operator-() const {
-    Point p;
-    p.x = -x;
-    p.y = -y;
-    return p;
+	Point p;
+	p.x = -x;
+	p.y = -y;
+	return p;
 }
 
 Point Point::operator*(const float value) const {
-    Point p;
-    p.x = x * value;
-    p.y = y * value;
-    return p;
+	Point p;
+	p.x = x * value;
+	p.y = y * value;
+	return p;
 }
 
 Point Point::operator*(const Point &other) const {
-    Point p;
-    p.x = x * other.x;
-    p.y = y * other.y;
-    return p;
+	Point p;
+	p.x = x * other.x;
+	p.y = y * other.y;
+	return p;
 }
 
-Point & Point::operator*=(const float value) {
-    x *= value;
-    y *= value;
-    return *this;
+Point &Point::operator*=(const float value) {
+	x *= value;
+	y *= value;
+	return *this;
 }
 
-Point & Point::operator*=(const Point &other) {
-    x *= other.x;
-    y += other.y;
-    return *this;
+Point &Point::operator*=(const Point &other) {
+	x *= other.x;
+	y += other.y;
+	return *this;
 }
 
 Point Point::operator/(const float value) const {
-    Point p;
-    p.x = x / value;
-    p.y = y / value;
-    return p;
+	Point p;
+	p.x = x / value;
+	p.y = y / value;
+	return p;
 }
 
 Point Point::operator/(const Point &other) const {
-    Point p;
-    p.x = x / other.x;
-    p.y = y / other.y;
-    return p;
+	Point p;
+	p.x = x / other.x;
+	p.y = y / other.y;
+	return p;
 }
 
-Point & Point::operator/=(const float value) {
-    x /= value;
-    y /= value;
-    return *this;
+Point &Point::operator/=(const float value) {
+	x /= value;
+	y /= value;
+	return *this;
 }
 
-Point & Point::operator/=(const Point &other) {
-    x /= other.x;
-    y /= other.y;
-    return *this;
+Point &Point::operator/=(const Point &other) {
+	x /= other.x;
+	y /= other.y;
+	return *this;
 }
 
 float Point::operator[](const int index) const {
-    if (index == 0)
-        return x;
-    else if (index == 1)
-        return y;
-    throw "Point: specified index is not valid.";
+	if (index == 0)
+		return x;
+	else if (index == 1)
+		return y;
+	throw "Point: specified index is not valid.";
 }
 
 bool Point::operator==(const Point &other) const {
-    return x == other.x && y == other.y;
+	return x == other.x && y == other.y;
 }
 
 bool Point::operator!=(const Point &other) const {
-    return !(*this == other);
+	return !(*this == other);
 }
--- point.h
+++ point.h
@@ -1,58 +1,57 @@
 #ifndef AIV_POINT_H
 #define AIV_POINT_H
 
-class Point
-{
+class Point {
 public:
-    Point();
-    Point(const float _x, const float _y);
-    Point(const Point &other);
+	Point();
+	Point(const float _x, const float _y);
+	Point(const Point &other);
 
-    float x;
+	float x;
 
-    float y;
+	float y;
 
-    Point & operator=(const Point &other);
+	Point &operator=(const Point &other);
 
-    Point operator+(const float value) const;
+	Point operator+(const float value) const;
 
-    Point operator+(const Point &other) const;
+	Point operator+(const Point &other) const;
 
-    Point & operator+=(const float value);
+	Point &operator+=(const float value);
 
-    Point & operator+=(const Point &other);
+	Point &operator+=(const Point &other);
 
-    Point operator-(const float value) const;
+	Point operator-(const float value) const;
 
-    Point operator-(const Point &other) const;
+	Point operator-(const Point &other) const;
 
-    Point & operator-=(const float value);
+	Point &operator-=(const float value);
 
-    Point & operator-=(const Point &other);
+	Point &operator-=(const Point &other);
 
-    Point operator-() const;
+	Point operator-() const;
 
-    Point operator*(const float value) const;
+	Point operator*(const float value) const;
 
-    Point operator*(const Point &other) const;
+	Point operator*(const Point &other) const;
 
-    Point & operator*=(const float value);
+	Point &operator*=(const float value);
 
-    Point & operator*=(const Point &other);
+	Point &operator*=(const Point &other);
 
-    Point operator/(const float value) const;
+	Point operator/(const float value) const;
 
-    Point operator/(const Point &other) const;
+	Point operator/(const Point &other) const;
 
-    Point & operator/=(const float value);
+	Point &operator/=(const float value);
 
-    Point & operator/=(const Point &other);
+	Point &operator/=(const Point &other);
 
-    float operator[](const int index) const;
+	float operator[](const int index) const;
 
-    bool operator==(const Point &other) const;
+	bool operator==(const Point &other) const;
 
-    bool operator!=(const Point &other) const;
+	bool operator!=(const Point &other) const;
 };
 
 #endif
